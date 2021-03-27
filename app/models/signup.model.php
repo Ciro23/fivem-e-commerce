0362658e263bdd80f4fbd38f235750fc60ff8f52
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-27 21:32:49 +0100

    added validateUsername() method

--- app/models/signup.model.php
+++ app/models/signup.model.php
@@ -11,5 +11,26 @@ class SignupModel extends Mvc\Model {
         $username = htmlspecialchars($username);
         $password = htmlspecialchars($password);
         $rePassword = htmlspecialchars($rePassword);
+
+
+    }
+
+    /**
+     * validate the username
+     * 
+     * @param string $username
+     * 
+     * @return string|false, first contains error on failure, false otherwise
+     */
+    private function validateUsername($username) {
+        if (empty($username)) {
+            return "username-cant-be-empty";
+        }
+
+        if (strlen($username) < 4 || strlen($username) > 20) {
+            return "username-length-must-be-between-4-and-20";
+        }
+
+        return false;
     }
 }
