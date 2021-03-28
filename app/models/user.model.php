87e8b2899befc04b1f5b537e8cceff20ac6b0f9c
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-28 17:18:10 +0200

    added getUserPassword() method

--- app/models/user.model.php
+++ app/models/user.model.php
@@ -1,5 +1,23 @@
 <?php
 
 class UserModel extends Mvc\Model {
-    
+
+    /**
+     * gets the user password given the email
+     * 
+     * @param string $email
+     * 
+     * @return string|bool the email or false on failure
+     */
+    public function getUserPassword($email) {
+        $sql = "SELECT password FROM user WHERE email = ?";
+        $inParameters = [$email];
+
+        // tries to run the query
+        if ($query = $this->executeStmt($sql, $inParameters)) {
+            return $query->fetch(PDO::FETCH_COLUMN);
+        }
+        $this->error = true;
+        return false;
+    }
 }
