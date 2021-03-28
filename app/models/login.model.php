e02984327a10843c2709daff0f1e4aaf812a0213
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-28 15:39:15 +0200

    initial upload

---
+++ app/models/login.model.php
@@ -0,0 +1,13 @@
+<?php
+
+class LoginModel extends Mvc\Model {
+
+    /**
+     * performs the login action
+     * 
+     * @return bool success status
+     */
+    public function login() {
+
+    }
+}
