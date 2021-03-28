e38fd6efc9af6db23bce960f9b6d2a1a91c77d9c
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-28 15:44:17 +0200

    gets user input and sanitizes it

--- app/models/login.model.php
+++ app/models/login.model.php
@@ -8,6 +8,11 @@ class LoginModel extends Mvc\Model {
      * @return bool success status
      */
     public function login() {
+        // gets user input from the html form
+        extract($_POST);
 
+        // sanitizes the input
+        $email = htmlspecialchars($email);
+        $password = htmlspecialchars($password);
     }
 }
