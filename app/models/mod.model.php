09f7568a847a0b86e5a6db3f603f10509310ebd9
Ciro Sorre <ciro.sorre23@gmail.com>
2021-03-31 16:27:59 +0200

    added upload() method

--- app/models/mod.model.php
+++ app/models/mod.model.php
@@ -2,4 +2,54 @@
 
 class ModModel extends Mvc\Model {
 
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var string $description
+     */
+    private $description;
+
+    /**
+     * @var string $version
+     */
+    private $version;
+
+    /**
+     * @var int $author
+     */
+    private $author;
+
+    /**
+     * @var array $file
+     */
+    private $file;
+
+    /**
+     * @var array $image
+     */
+    private $image;
+
+    /**
+     * performs the upload action
+     */
+    public function upload() {
+        // gets user input
+        extract($_POST);
+
+        // sanitizes user input
+        $name = htmlspecialchars($name);
+        $description = htmlspecialchars($description);
+        $version = htmlspecialchars($version);
+
+        // saves data into class properties
+        $this->name = $name;
+        $this->description = $description;
+        $this->version = $version;
+        $this->author = $_SESSION['uid'];
+        $this->file = $_FILES['file']['tmp_name'];
+        $this->image = $_FILES['image']['tmp_name'];
+    }
 }
