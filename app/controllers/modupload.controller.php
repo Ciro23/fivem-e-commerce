d5ce28c1e1438abf306a8d7a051e4225e3072b54
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-01 20:29:08 +0200

    gets get data in case of error

--- app/controllers/modupload.controller.php
+++ app/controllers/modupload.controller.php
@@ -7,7 +7,16 @@ class ModUploadController extends Mvc\Controller {
      */
     public function index() {
         if (isset($_SESSION['uid'])) {
-            $this->view("modupload");
+            // saves the error, the email if something goes wrong
+            $data['form']['error'] = $_GET['error'] ?? "";
+            $data['form']['name'] = $_GET['name'] ?? "";
+            $data['form']['description'] = $_GET['description'] ?? "";
+            $data['form']['version'] = $_GET['version'] ?? "";
+
+            // formats the error
+            $data['form']['error'] = SignupModel::formatError($data['form']['error']);
+
+            $this->view("modupload", $data);
         } else {
             header("Location: /login");
         }
