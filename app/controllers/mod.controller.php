ed6377cacc2cbccd551b992a7d846523477ea55f
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-01 10:47:10 +0200

    added the upload() method

--- app/controllers/mod.controller.php
+++ app/controllers/mod.controller.php
@@ -2,4 +2,29 @@
 
 class ModController extends Mvc\Controller {
 
+    /**
+     * performs the upload action
+     */
+    public function upload() {
+        // if user user is logged in
+        if (isset($_SESSION['uid'])) {
+            $modModel = $this->model("mod");
+
+            // tries to upload the mod
+            if ($modModel->upload()) {
+                header("Location: /");
+            } else {
+                header("Location: /mod/upload/?error="
+                    . $modModel->error
+                    . "&name="
+                    . $modModel->name
+                    . "&description="
+                    . $modModel->description
+                    . "&version="
+                    . $modModel->version);
+            }
+        } else {
+            header("Location: /");
+        }
+    }
 }
