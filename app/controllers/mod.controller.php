3f0ab48a23d6838dcc27facdbdb63aa858d9cc4f
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-01 10:50:42 +0200

    removed session check

--- app/controllers/mod.controller.php
+++ app/controllers/mod.controller.php
@@ -6,25 +6,20 @@ class ModController extends Mvc\Controller {
      * performs the upload action
      */
     public function upload() {
-        // if user user is logged in
-        if (isset($_SESSION['uid'])) {
-            $modModel = $this->model("mod");
+        $modModel = $this->model("mod");
 
-            // tries to upload the mod
-            if ($modModel->upload()) {
-                header("Location: /");
-            } else {
-                header("Location: /mod/upload/?error="
-                    . $modModel->error
-                    . "&name="
-                    . $modModel->name
-                    . "&description="
-                    . $modModel->description
-                    . "&version="
-                    . $modModel->version);
-            }
-        } else {
+        // tries to upload the mod
+        if ($modModel->upload()) {
             header("Location: /");
+        } else {
+            header("Location: /mod/upload/?error="
+                . $modModel->error
+                . "&name="
+                . $modModel->name
+                . "&description="
+                . $modModel->description
+                . "&version="
+                . $modModel->version);
         }
     }
 }
