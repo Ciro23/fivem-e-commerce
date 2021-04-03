f9ed99134b2663022502881d9728a3ae9f9d0800
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-03 11:35:10 +0200

    added doesNameExists() method

--- app/models/mod.model.php
+++ app/models/mod.model.php
@@ -2,4 +2,19 @@
 
 class ModModel extends Mvc\Model {
     
+    public function doesNameExists($name) {
+        $sql = "SELECT COUNT(*) FROM mods WHERE name = ?";
+        $query = $this->executeStmt($sql, [$name]);
+
+        // tries to run the query
+        if ($query) {
+            // checks if the email is already registered
+            if ($query->fetch(PDO::FETCH_COLUMN) == 1) {
+                return true;
+            }
+            return false;
+        }
+        $this->PDOError = true;
+        return false;
+    }
 }
