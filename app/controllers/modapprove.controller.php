93530a77b6ecd4e8544f5751ddad4617f4bfb90c
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-03 20:29:39 +0200

    added updateStatus() method

--- app/controllers/modapprove.controller.php
+++ app/controllers/modapprove.controller.php
@@ -2,4 +2,19 @@
 
 class ModApproveController extends Mvc\Controller {
     
+    /**
+     * updates the mod status
+     * 
+     * @param int $modId
+     * @param int $status (0 => rejected, 1 => pending, 2 => approved)
+     */
+    public function updateStatus($modId, $status) {
+        $modApproveModel = $this->model("ModApprove");
+
+        if ($modApproveModel->updateStatus($modId, $status)) {
+            header("Location: /mod/" . $modId);
+        } else {
+            header("Location: /mod/" . $modId . "/?error=" . $modApproveModel->error);
+        }
+    }
 }
