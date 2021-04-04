7a5cb28a6abde5f50fa59b3e04cf281078bb1dfc
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-04 14:43:15 +0200

    added getDetails() method

--- app/models/mod.model.php
+++ app/models/mod.model.php
@@ -2,6 +2,25 @@
 
 class ModModel extends Mvc\Model {
     
+    /**
+     * gets mod details
+     * 
+     * @param int $id
+     * 
+     * @return array|false first contains mod data, false in case of error
+     */
+    public function getDetails($id) {
+        $sql = "SELECT * FROM mods WHERE id = ?";
+        $query = $this->executeStmt($sql, [$id]);
+
+        // tries to run the query
+        if ($query) {
+            // returns an associative array with the mod data
+            return $query->fetch(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
     public function doesNameExists($name) {
         $sql = "SELECT COUNT(*) FROM mods WHERE name = ?";
         $query = $this->executeStmt($sql, [$name]);
