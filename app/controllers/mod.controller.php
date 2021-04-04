98cc35c456eed85df8da50b072da097846db06d0
Ciro Sorre <ciro.sorre23@gmail.com>
2021-04-04 14:22:31 +0200

    added index() method

--- app/controllers/mod.controller.php
+++ app/controllers/mod.controller.php
@@ -2,4 +2,21 @@
 
 class ModController extends Mvc\Controller {
     
+    /**
+     * shows the mod page
+     * 
+     * @param int $modId
+     */
+    public function index($modId) {
+        $modModel = $this->model("Mod");
+
+        // gets the mod data
+        $data = $modModel->getData($modId);
+
+        if ($data !== null) {
+            $this->view("mod", $data);
+        } else {
+            $this->view("pagenotfound");
+        }
+    }
 }
