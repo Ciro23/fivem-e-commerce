99c7320b777acd078190416e0f4b4201b96d56ce
Ciro Sorre <ciro.sorre23@gmail.com>
2021-05-21 17:37:56 +0200

    adapted to work with codeigniter

--- app/Models/SignupModel.php
+++ app/Models/SignupModel.php
@@ -13,41 +13,24 @@ class SignupModel extends Model {
         "rePassword" => ""
     ];
 
-    public function signup(array $inputData, UserModel $userModel): bool {
+    public function signup(array $inputData): void {
         // gets the form input
         $this->userData = \InputHelper::getFormInput($this->userData, $inputData);
-
-        // checks for errors
-        if (
-            !$this->isUserEmailValid($userModel)
-            || !$this->isUsernameValid()
-            || !$this->isUserPasswordValid()
-        ) {
-            return false;
-        }
 
         // hashes the password
         $this->userData['password'] = password_hash($this->userData['password'], PASSWORD_BCRYPT);
 
-        // insert the user userData into the db and creates the session
-        if ($this->insertIntoDb()) {
-            $_SESSION['uid'] = $this->lastInsertId("users");
-            return true;
-        }
-        // in case of query failure
-        $this->signupError = "something-went-wrong";
-        return false;
+        $this->insertIntoDb();
     }
 
-    private function insertIntoDb(): bool {
-        $sql = "INSERT INTO users (email, username, password) VALUES (?, ?, ?)";
-        $params = [$this->userData['email'], $this->userData['username'], $this->userData['password']];
+    private function insertIntoDb(): void {
+        $data = [
+            "email" => $this->userData['email'],
+            "username" => $this->userData['username'],
+            "password" => $this->userData['password']
+        ];
 
-        // tries to run the query
-        if ($this->executeStmt($sql, $params)) {
-            return true;
-        }
-        
-        return false;
+        $builder = $this->db->table("users")
+                            ->insert($data);
     }
 }
