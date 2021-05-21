b2ad70773f629332e8cdb73d91431b85bd5ecf53
Ciro Sorre <ciro.sorre23@gmail.com>
2021-05-21 20:01:17 +0200

    added is_unique() method

--- app/Validation/UserRules.php
+++ app/Validation/UserRules.php
@@ -2,6 +2,21 @@
 
 namespace App\Validation;
 
+use App\Models\UserModel;
+
 class UserRules {
     
+    /**
+     * checks if a specified user field is unique
+     * 
+     * @param string $parameter the user field to check
+     * @param string $value the value of the field
+     * 
+     * @return bool
+     */
+    public function is_unique(string $parameter, string $value): bool {
+        $userModel = new UserModel;
+
+        return $userModel->doesUserExists($parameter, $value);
+    }
 }
