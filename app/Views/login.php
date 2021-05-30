689e6417a95da75045e2c2060a7fb96dbba90d1e
Ciro Sorre <ciro.sorre23@gmail.com>
2021-05-30 20:26:33 +0200

    created a nice to see login page

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,20 +1,19 @@
-<?php include_once __DIR__ . "/../included/header.included.php" ?>
+<div class="login-signup">
+    <form method="POST">
+        <h2>Login</h2>
 
-    <!-- this page's css -->
-    <link rel="stylesheet" href="/assets/styles/css/login-signup.style.css">
+        <?php 
+        if (isset($errors)) {
+            echo $errors;
+        }
+        ?>
 
-    <title>Login - <?= $_ENV['site_name'] ?></title>
-</head>
-<body>
-    <?php include_once __DIR__ . "/../included/navbar.included.php" ?>
-    
-    <div class="login-signup">
-        <form action="/login/action" method="POST">
-            <h2>Login on <?= $_ENV['site_name'] ?></h2>
-            <input type="text" placeholder="email" name="email">
-            <input type="password" placeholder="password" name="password">
-            <input type="submit" value="login">
-        </form>
-    </div>
-</body>
-</html>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email">
+
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password">
+        
+        <input type="submit" value="Login">
+    </form>
+</div>
