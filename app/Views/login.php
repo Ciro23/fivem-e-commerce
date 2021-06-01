5202a144162f84cd4f3d6c5f4f7a0d5fbfd99e1d
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-01 19:28:17 +0200

    added link to switch from login to signup and viceversa

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,19 +1,22 @@
 <div class="login-signup">
-    <form method="POST">
-        <h2>Login</h2>
+    <div>
+        <form method="POST">
+            <h2>Login</h2>
 
-        <?php 
-        if (isset($errors)) {
-            echo $errors;
-        }
-        ?>
+            <?php
+            if (isset($errors)) {
+                echo $errors;
+            }
+            ?>
 
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email">
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
-        
-        <input type="submit" value="Login">
-    </form>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+
+            <input type="submit" value="Login">
+        </form>
+        <a href="/signup">Don't have an account? Signup</a>
+    </div>
 </div>
--- app/Views/signup.php
+++ app/Views/signup.php
@@ -1,31 +1,34 @@
 <div class="login-signup">
-    <form method="POST">
-        <h2>Signup</h2>
+    <div>
+        <form method="POST">
+            <h2>Signup</h2>
 
-        <?php 
-        if (isset($errors)) {
-            echo $errors;
-        }
-        ?>
+            <?php 
+            if (isset($errors)) {
+                echo $errors;
+            }
+            ?>
 
-        <div class="form-columns">
-            <div>
-                <label for="email">Email</label>
-                <input type="text" id="email" name="email">
+            <div class="form-columns">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email">
 
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username">
+                </div>
+
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password">
+
+                    <label for="confirm_password">Confirm password</label>
+                    <input type="password" id="confirm_password" name="confirm_password">
+                </div>
             </div>
-
-            <div>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password">
-
-                <label for="confirm_password">Confirm password</label>
-                <input type="password" id="confirm_password" name="confirm_password">
-            </div>
-        </div>
-        
-        <input type="submit" value="Signup">
-    </form>
+            
+            <input type="submit" value="Signup">
+        </form>
+        <a href="/signup">Already registered? Login</a>
+    </div>
 </div>
