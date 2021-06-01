0cf934466da10f26ead1da9e8196363ca5b9a799
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-01 19:36:57 +0200

    index() now only calls the view

--- app/Controllers/Mod/ModUploadController.php
+++ app/Controllers/Mod/ModUploadController.php
@@ -12,20 +12,7 @@ class ModUploadController extends BaseController {
      * shows the upload mod page only if the user is logged in
      */
     public function index(): void {
-        if (isset($_SESSION['uid'])) {
-            // saves the error, and all the form data if something goes wrong
-            $data['form']['error'] = $_GET['error'] ?? "";
-            $data['form']['name'] = $_GET['name'] ?? "";
-            $data['form']['description'] = $_GET['description'] ?? "";
-            $data['form']['version'] = $_GET['version'] ?? "";
-
-            // formats the error
-            $data['form']['error'] = \StringHelper::formatError($data['form']['error']);
-
-            echo view("modupload", $data);
-        } else {
-            header("Location: /login");
-        }
+        echo view("mod/mod_upload");
     }
 
     public function uploadMod(): void {
