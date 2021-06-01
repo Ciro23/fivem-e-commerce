82a2368fe9a5369a3747edcddbcf323919f2e2b9
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-01 19:51:09 +0200

    created a nice too see mod upload page

--- app/Views/mod/mod_upload.php
+++ app/Views/mod/mod_upload.php
@@ -1,18 +1,22 @@
-<?php include_once __DIR__ . "/../included/header.included.php" ?>
+<div class="container">
+    <form method="POST">
+        <h2>Upload a mod</h2>
 
-    <!-- this page's css -->
-    <link rel="stylesheet" href="/assets/styles/css/login.style.css">
+        <?php
+        if (isset($errors)) {
+            echo $errors;
+        }
+        ?>
 
-    <title>Upload mod - <?= $_ENV['site_name'] ?></title>
-</head>
-<body>
-    <form action="/mod/upload/action" method="POST" enctype="multipart/form-data">
-        <input type="text" name="name" placeholder="name" value="<?= $data['form']['name'] ?>">
-        <input type="text" name="description" placeholder="description" value="<?= $data['form']['description'] ?>">
-        <input type="text" name="version" placeholder="version" value="<?= $data['form']['version'] ?>">
-        <input type="file" name="file" placeholder="file">
-        <input type="file" name="image" placeholder="image">
-        <input type="submit" value="upload">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name">
+
+        <label for="description">description</label>
+        <input type="text" id="description" name="description">
+
+        <label for="version">Version</label>
+        <input type="text" id="version" name="version">
+
+        <input type="submit" value="Upload mod">
     </form>
-</body>
-</html>
+</div>
