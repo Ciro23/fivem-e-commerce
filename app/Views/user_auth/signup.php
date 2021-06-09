75d97e465ddb5d2e25fafa248f0ca7c92208a6fe
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-09 21:36:08 +0200

    added placeholders

--- app/Views/user_auth/signup.php
+++ app/Views/user_auth/signup.php
@@ -12,15 +12,15 @@
             <div class="form-columns">
                 <div>
                     <label for="email">Email</label>
-                    <input type="text" id="email" name="email">
+                    <input type="text" id="email" name="email" placeholder="example@gmail.com">
 
                     <label for="username">Username</label>
-                    <input type="text" id="username" name="username">
+                    <input type="text" id="username" name="username" placeholder="4-20 alpha numeric chars">
                 </div>
 
                 <div>
                     <label for="password">Password</label>
-                    <input type="password" id="password" name="password">
+                    <input type="password" id="password" name="password" placeholder="6-72 chars">
 
                     <label for="confirm_password">Confirm password</label>
                     <input type="password" id="confirm_password" name="confirm_password">
