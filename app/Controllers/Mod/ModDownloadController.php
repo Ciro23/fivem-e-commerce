019a97991e67cc57d36e0195decd47deb77a019f
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-24 16:42:27 +0200

    created ModDownloadController and download method

---
+++ app/Controllers/Mod/ModDownloadController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers\Mod;
+
+use App\Controllers\BaseController;
+use App\Models\Mod\ModModel;
+
+class ModDownloadController extends BaseController {
+
+    /**
+     * downloads the specified mod file
+     */
+    public function download(int $modId) {
+        $modModel = new ModModel();
+        $mod = $modModel->getModDetails($modId);
+
+        return $this->response->download(WRITEPATH . "uploads/mods_files/" . $modId . "/file." . $mod->file_ext, null)
+                              ->setFileName($mod->name . "." . $mod->file_ext);
+
+        // close the browser window of the download
+        echo "<script>window.close();</script>";
+    }
+}
