b549de6686c443d586dce34d4a18e734c0e415c1
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-24 16:42:47 +0200

    created the mod html page

--- app/Views/mod/mod.php
+++ app/Views/mod/mod.php
@@ -1,11 +1,48 @@
-<?php include_once __DIR__ . "/../included/header.included.php" ?>
+<div class="container">
+    <div class="heading">
+        <img src="/public/assets/mods_images/<?= $mod->id ?>/image.<?= $mod->image_ext ?>">
+        <h1><?= $mod->name ?></h1>
 
-    <!-- this page's css -->
-    <link rel="stylesheet" href="/assets/styles/css/login.style.css">
+        <div class="buttons">
+            <a href="<?= $mod->id ?>/download">Download</a>
+        </div>
+    </div>
 
-    <title><?= $data['mod']['name'] . "-" . $_ENV['site_name'] ?></title>
-</head>
-<body>
-    <div class="mega-box"></div>
-</body>
-</html>
+    <div class="info">
+        <div class="description">
+            <h3>Description</h3>
+            <p><?= $mod->description ?></p>
+        </div>
+
+        <div class="about">
+            <h4>About this mod</h4>
+
+            <ul>
+                <li>    
+                    <p>Author:</p>
+                    <p><?= $mod->author_name ?></p>
+                </li>
+
+                <li>
+                    <p>Price:</p>
+                    <p><?= $mod->price == 0 ? "Free" : $mod->price . "€"; ?></p>
+                </li>
+
+                <li>
+                    <p>Uploaded at:</p>
+                    <p><?= date("j M, Y", strtotime($mod->created_at)) ?></p>
+                </li>
+
+                <li>
+                    <p>Last update:</p>
+                    <p><?= date("j M, Y", strtotime($mod->updated_at)) ?></p>
+                </li>
+
+                <li>
+                    <p>Mod size:</p>
+                    <p><?= $mod->size ?> bytes</p>
+                </li>
+            </div>
+        </div>
+    </div>
+</div>
