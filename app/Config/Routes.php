adff5994c7309a1e21921978bd3dba0a20da6c18
Ciro Sorre <ciro.sorre23@gmail.com>
2021-06-29 10:35:24 +0200

    removed the useless slash at the beginning of mod routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -50,16 +50,16 @@ $routes->get("/logout", "UserAuth\LoginController::logout");
 
 // mod routes
 $routes->group("", ["namespace" => "App\Controllers\Mod"], function ($routes) {
-	$routes->get("/mod/(:num)", "ModController::index/$1");
+	$routes->get("mod/(:num)", "ModController::index/$1");
 
-	$routes->get("/upload/mod", "ModUploadController::index");
-	$routes->post("/upload/mod", "ModUploadController::uploadMod");
+	$routes->get("upload/mod", "ModUploadController::index");
+	$routes->post("upload/mod", "ModUploadController::uploadMod");
 
-	$routes->get("/manage/mods", "ModManageController::index");
-	$routes->get("/approve/mod/(:num)", "ModManageController::approve/$1");
-	$routes->get("/deny/mod/(:num)", "ModManageController::deny/$1");
+	$routes->get("manage/mods", "ModManageController::index");
+	$routes->get("approve/mod/(:num)", "ModManageController::approve/$1");
+	$routes->get("deny/mod/(:num)", "ModManageController::deny/$1");
 
-	$routes->get("/download/mod/(:num)", "ModDownloadController::download/$1");
+	$routes->get("download/mod/(:num)", "ModDownloadController::download/$1");
 });
 
 /*
