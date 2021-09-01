26f55543eb5e46f3f5b6ea5646c331747b54e1ce
Ciro Sorre <ciro.sorre23@gmail.com>
2021-09-01 18:04:54 +0200

    document formatting

--- app/Views/mod/manage.php
+++ app/Views/mod/manage.php
@@ -1,8 +1,6 @@
 <div class="container distance max-w-5xl mx-auto flex flex-col space-y-10">
     <h1 class="font-medium text-xl text-center">List of all pending mods</h1>
-    <?php
-    foreach ($mods as $mod) :
-    ?>
+    <?php foreach ($mods as $mod) : ?>
 
 
         <div class="flex h-48 shadow-md rounded p-6">
