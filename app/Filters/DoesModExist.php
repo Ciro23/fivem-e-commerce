cff3d997dbab4ed8950eaea0968611a4ef863da0
Ciro Sorre <ciro.sorre23@gmail.com>
2021-09-01 12:56:03 +0200

    the mod id now is always the second segment of the url

--- app/Filters/DoesModExist.php
+++ app/Filters/DoesModExist.php
@@ -11,15 +11,11 @@ use CodeIgniter\HTTP\ResponseInterface;
 class DoesModExist implements FilterInterface {
 
     public function before(RequestInterface $request, $arguments = null) {
-        // $segments will be something like ['download', 'mod', '10']
+        // gets the mod id from the url
+        // url will be like this ['mod', '10', ...]
         $uri = &$request->uri;
         $segments = array_filter($uri->getSegments());
-
-        for ($i = 0; $i < count($segments); $i++) {
-            if ($segments[$i] == "mod") {
-                $id = $segments[$i + 1];
-            }
-        }
+        $id = $segments[1];
 
         $modModel = new ModModel();
 
