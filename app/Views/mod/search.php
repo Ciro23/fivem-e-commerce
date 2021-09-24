7b7966b5c75d032a9d5cdb65c55d8a44e896f489
ciro23 <ciro.sorre23@gmail.com>
2021-09-24 13:35:44 +0200

    initial upload

---
+++ app/Views/mod/search.php
@@ -0,0 +1,12 @@
+<div class="container distance max-w-5xl mx-auto flex flex-col space-y-10">
+    <h1 class="font-medium text-xl text-center">Results for "<?= esc($query) ?>"</h1>
+    <?php if (empty($mods)) : ?>
+        <p class="text-center">wow! such empty :C</p>
+    <?php endif ?>
+
+    <?php
+    foreach ($mods as $mod) {
+        echo view_cell("\App\Libraries\Mod::modPreview", ["mod" => $mod]);
+    }
+    ?>
+</div>
