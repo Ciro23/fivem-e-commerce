3ecc01f5e76f29bb72c4cc0a41f90f0e1a2feb38
ciro23 <ciro.sorre23@gmail.com>
2021-09-25 12:16:30 +0200

    initial upload

--- app/Views/mod/edit.php
+++ app/Views/mod/edit.php
@@ -1,3 +1,38 @@
 <?= view_cell("\App\Libraries\ViewCells::header") ?>
 
+<div class="mega-box">
+    <form method="POST" enctype="multipart/form-data">
+        <h2>Update mod</h2>
+
+        <?php
+        if (isset($errors)) {
+            echo $errors;
+        }
+        ?>
+
+        <div class="form-columns">
+            <div>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" value="<?= $mod->name ?>" placeholder="4-30 chars (a-z, 0-9, spaces)">
+
+                <label for="price">Price in Euros</label>
+                <input type="number" id="price" name="price" value="<?= $mod->price ?>" placeholder="9,99" min="0" max="25">
+            </div>
+
+            <div>
+                <label for="file">File</label>
+                <input type="file" id="file" name="file">
+
+                <label for="image">Image</label>
+                <input type="file" id="image" name="image">
+            </div>
+        </div>
+
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="5" placeholder="10-10000 characters" minlength="10" maxlength="10000"><?= $mod->name ?></textarea>
+
+        <input type="submit" value="Update mod">
+    </form>
+</div>
+
 <?= view_cell("\App\Libraries\ViewCells::footer") ?>
