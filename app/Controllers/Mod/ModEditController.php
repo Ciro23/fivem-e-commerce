f202eb19d7f53f63d5a2b234d19c3208ade43e56
Ciro Sorre <ciro.sorre23@gmail.com>
2021-09-26 19:13:52 +0200

    added editMod()

--- app/Controllers/Mod/ModEditController.php
+++ app/Controllers/Mod/ModEditController.php
@@ -19,4 +19,49 @@ class ModEditController extends BaseController {
 
         echo view("mod/edit", $this->data);
     }
+
+    public function editMod(int $modId) {
+        helper("form");
+
+        if ($this->validate("mod_edit")) {
+            $modModel = new ModModel;
+            $modDetails = $modModel->getModDetails($modId);
+
+            $file = $this->request->getFile("file");
+            $image = $this->request->getFile("image");
+
+            // if no file or image is uploaded, keep the old ones
+            $fileExt = $modDetails->file_ext;
+            $fileSize = $modDetails->size;
+            $imageExt = $modDetails->image_ext;
+
+            if ($file->isValid() && !$file->hasMoved()) {
+                $fileExt = $file->getExtension();
+                $fileSize = $file->getSize();
+
+                $file->move(WRITEPATH . "uploads/mods_files/" . $modId, "file." . $fileExt);
+            }
+
+            if ($image->isValid() && !$image->hasMoved()) {
+                $imageExt = $image->getExtension();
+
+                $image->move(ROOTPATH . "/public/assets/mods_images/" . $modId, "image." . $imageExt);
+            }
+
+            $additionalData = [
+                "size" => $fileSize,
+                "file_ext" => $fileExt,
+                "image_ext" => $imageExt,
+            ];
+
+            $data = array_merge($this->request->getPost(), $additionalData);
+            $modModel->update($modDetails->id, $data);
+
+            return redirect("home");
+        }
+
+        $this->data['errors'] = $this->validator->listErrors("custom_errors");
+
+        $this->index($modId);
+    }
 }
