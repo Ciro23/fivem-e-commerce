398b17dd9f45518e5e69e870a321e9cbc80faa64
ciro23 <ciro.sorre23@gmail.com>
2021-10-05 12:32:16 +0200

    initial upload

---
+++ app/Filters/IsUserProfileOwner.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filters;
+
+use App\Models\User\UserModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class IsUserProfileOwner implements FilterInterface {
+
+    public function before(RequestInterface $request, $arguments = null) {
+        // gets the mod id from the url
+        // url will be like this ['user', '10', ...]
+        $uri = &$request->uri;
+        $segments = array_filter($uri->getSegments());
+        $id = $segments[1];
+
+        $modModel = new UserModel();
+
+        if ($modModel->getUserDetails($id)->id != session("uid")) {
+            throw new PageNotFoundException();
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+    }
+}
