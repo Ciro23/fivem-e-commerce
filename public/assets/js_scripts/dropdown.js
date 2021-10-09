8d14368ac3980b77dfac7db83b3866f33a1b722c
ciro23 <ciro.sorre23@gmail.com>
2021-10-09 11:34:52 +0200

    removed document.onload

--- public/assets/js_scripts/dropdown.js
+++ public/assets/js_scripts/dropdown.js
@@ -1,16 +1,14 @@
-window.onload = function() {
-    const navbar = document.getElementById("dropdown");
-    const dropdownContent = document.getElementById("dropdown-content");
+const navbar = document.getElementById("dropdown");
+const dropdownContent = document.getElementById("dropdown-content");
 
-    dropdownContent.style.display = "none";
+dropdownContent.style.display = "none";
 
-    document.addEventListener("click", function(event) {
-        if (event.target == navbar
-            && dropdownContent.style.display == "none"
-        ) {
-            dropdownContent.style.display = "flex";
-        } else {
-            dropdownContent.style.display = "none";
-        }
-    });
-};
+document.addEventListener("click", function(event) {
+    if (event.target == navbar
+        && dropdownContent.style.display == "none"
+    ) {
+        dropdownContent.style.display = "flex";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+});
--- public/assets/js_scripts/mod_search.js
+++ public/assets/js_scripts/mod_search.js
@@ -1,9 +1,7 @@
-window.onload = function() {
-    const modSearchInput = document.getElementById("mod-search-input");
+const modSearchInput = document.getElementById("mod-search-input");
 
-    modSearchInput.addEventListener("keyup", function(event) {
-        if (event.key == "Enter") {
-            window.location.href = "/search/" + modSearchInput.value;
-        }
-    });
-};
+modSearchInput.addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        window.location.href = "/search/" + modSearchInput.value;
+    }
+});
