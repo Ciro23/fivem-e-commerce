28cc79674b5e89465a3ce4ca9c963707d6451c70
ciro23 <ciro.sorre23@gmail.com>
2021-10-09 11:26:24 +0200

    initial upload

---
+++ public/assets/js_scripts/dropdown.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    const navbar = document.getElementById("dropdown");
+    const dropdownContent = document.getElementById("dropdown-content");
+
+    dropdownContent.style.display = "none";
+
+    document.addEventListener("click", function(event) {
+        if (event.target == navbar
+            && dropdownContent.style.display == "none"
+        ) {
+            dropdownContent.style.display = "flex";
+        } else {
+            dropdownContent.style.display = "none";
+        }
+    });
+};
