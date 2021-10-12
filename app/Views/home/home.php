bb190fbdf166297aa961ac5f2172d310b4911d9d
ciro23 <ciro.sorre23@gmail.com>
2021-10-12 12:15:27 +0200

    added link for the mods page

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -2,7 +2,7 @@
 
 <div class="w-screen h-screen flex flex-col items-center justify-center space-y-4">
     <h1 class="text-3xl">Fivem e-commerce</h1>
-    <a href="" class="bg-yellow-500 text-white px-5 py-2.5 rounded-lg">Browse mods</a>
+    <a href="/mods" class="bg-yellow-500 text-white px-5 py-2.5 rounded-lg">Browse mods</a>
 </div>
 
 <?= view_cell("\App\Libraries\ViewCells::footer") ?>
