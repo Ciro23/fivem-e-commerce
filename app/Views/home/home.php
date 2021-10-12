4f6e1d8bcd50492e8bd1ad3f0958396def2d9e0d
ciro23 <ciro.sorre23@gmail.com>
2021-10-12 12:23:23 +0200

    css changes

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -1,8 +1,8 @@
 <?= view_cell("\App\Libraries\ViewCells::header") ?>
 
 <div class="w-screen h-screen flex flex-col items-center justify-center space-y-4">
-    <h1 class="text-3xl">Fivem e-commerce</h1>
-    <a href="/mods" class="bg-yellow-500 text-white px-5 py-2.5 rounded-lg">Browse mods</a>
+    <h1 class="text-3xl font-extralight">Fivem e-commerce</h1>
+    <a href="/mods" class="bg-yellow-500 text-white px-5 py-2.5 rounded text-sm">Browse mods</a>
 </div>
 
 <?= view_cell("\App\Libraries\ViewCells::footer") ?>
