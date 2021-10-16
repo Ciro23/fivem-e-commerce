37845d4be1c0693c696485dc5ca91d04bf981f73
ciro23 <ciro.sorre23@gmail.com>
2021-10-16 10:32:35 +0200

    initial upload

---
+++ app/Filters/DoesUserExist.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filters;
+
+use App\Models\User\UserModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class DoesUserExist implements FilterInterface {
+
+    public function before(RequestInterface $request, $arguments = null) {
+        // gets the mod id from the url
+        // url will be like this ['mod', '10', ...]
+        $uri = service("uri");
+        $segments = $uri->getSegments();
+        $id = $segments[1];
+
+        $userModel = new UserModel();
+
+        if (!$userModel->doesUserExist("id", $id)) {
+            throw new PageNotFoundException();
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+    }
+}
