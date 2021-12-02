88827ac5fe399fed0340fae18e6224aceed72e8b
ciro23 <ciro.sorre23@gmail.com>
2021-12-02 11:36:19 +0100

    escaped output

--- app/Views/mod/upload.php
+++ app/Views/mod/upload.php
@@ -13,10 +13,10 @@
         <div class="form-columns">
             <div>
                 <label for="name">Name (cannot be changed)*</label>
-                <input type="text" id="name" name="name" value="<?= $mod->name ?? "" ?>" placeholder="4-30 chars (a-z, 0-9, spaces)">
+                <input type="text" id="name" name="name" value="<?= esc($mod->name) ?? "" ?>" placeholder="4-30 chars (a-z, 0-9, spaces)">
 
                 <label for="price">Price in Euros*</label>
-                <input type="number" id="price" name="price" value="<?= $mod->price ?? "" ?>" placeholder="9,99" min="0" max="25">
+                <input type="number" id="price" name="price" value="<?= esc($mod->price) ?? "" ?>" placeholder="9,99" min="0" max="25">
             </div>
 
             <div>
@@ -37,7 +37,7 @@
         </div>
 
         <label for="description">Description*</label>
-        <textarea name="description" id="description" rows="5" placeholder="10-10000 characters" minlength="10" maxlength="10000"><?= $mod->description ?? "" ?></textarea>
+        <textarea name="description" id="description" rows="5" placeholder="10-10000 characters" minlength="10" maxlength="10000"><?= esc($mod->description) ?? "" ?></textarea>
 
         <input type="submit" value="Upload mod">
     </form>
